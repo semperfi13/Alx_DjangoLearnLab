4571d990243a2815377261ec1ce1c4b897217a71
adamou <adamsnikiema187@gmail.com>
2025-02-16 00:26:50 +0000

    1. Implementing and Interacting with Django Models

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
     def __str__(self):
