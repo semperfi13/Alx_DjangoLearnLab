2bc90f28c22ffecb1c51855108b3c9b3e3aedbe0
adamou <adamsnikiema187@gmail.com>
2025-03-02 12:06:33 +0000

    1. Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,8 +11,8 @@ def my_view(request):
 
 @login_required
 @permission_required("bookshelf.can_view", raise_exception=True)
-def list_books(request):
-    template_name = "relationship_app/list_books.html"
+def book_list(request):
+    template_name = "bookshelf/list_books.html"
     books = Book.objects.all()
     context = {"books": books}
 
