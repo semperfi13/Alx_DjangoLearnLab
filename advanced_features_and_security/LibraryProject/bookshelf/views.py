8d09fb2fbf2cec555ba03da3b762aadb51d0bdc2
adamou <adamsnikiema187@gmail.com>
2025-03-02 13:51:54 +0000

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
+from .forms import ExampleForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -17,3 +19,23 @@ def book_list(request):
     context = {"books": books}
 
     return render(request, template_name, context)
+
+
+def get_name(request):
+    template_name = "bookshelf/form_example.html"
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = ExampleForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ExampleForm()
+
+    return render(request, template_name, {"form": form})
