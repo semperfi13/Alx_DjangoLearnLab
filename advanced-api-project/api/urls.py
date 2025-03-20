189bdfb6d32d721a9fffb089ca4f2da2eda175f9
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-20 21:59:37 +0000

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CustomBookListView, CreateView, DetailView, UpdateView
+from .views import CustomBookListView, CreateView, DetailView, UpdateView, DeleteView
 
 urlpatterns = [
     path("books/", CustomBookListView.as_view(), name="books"),
     path("books/create", CreateView.as_view(), name="books-create"),
     path("books/retrieve/<pk>/", DetailView.as_view(), name="books-create"),
     path("books/update/<pk>/", UpdateView.as_view(), name="books-update"),
-    path("books/delete/<pk>/", UpdateView.as_view(), name="books-delete"),
+    path("books/delete/<pk>/", DeleteView.as_view(), name="books-delete"),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 
@@ -17,15 +18,18 @@ class CreateView(generics.CreateAPIView):
 
 
 class DetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class UpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class DeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
