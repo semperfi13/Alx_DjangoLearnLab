e546aa5b029850471bbb3fd4b79b2c645c68154c
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-22 00:30:02 +0000

    3. Writing Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -7,7 +7,12 @@ client = APIClient()
 client.post("/books/", {"title": "new idea"}, format="json")
 
 
-class BokkTestCase:
+class APITestCase(TestCase):
+
     def setUp(self):
         Book.objects.create(title="LION", publication_year=1980)
         Book.objects.create(title="CAT", publication_year=2024)
+
+    def test_book(self):
+        book = Book.objects.get(title="LION")
+        self.assertEqual(book.title, "LION")
