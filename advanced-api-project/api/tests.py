1e11709a07c473226717339e6387c8ab20526b37
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-22 00:37:58 +0000

    3. Writing Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,13 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase, URLPatternsTestCase
 from api.models import Book
+from rest_framework import status
+from django.urls import include, path, reverse
+
+client = APIClient()
+client.post("/books/", {"title": "new idea"}, format="json")
 
 
-# Create your tests here.
-class APITestCase(TestCase):
+class BokkTestCase(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        path("api/", include("api.urls")),
+    ]
+
     def setUp(self):
         Book.objects.create(title="LION", publication_year=1980)
         Book.objects.create(title="CAT", publication_year=2024)
 
     def test_book(self):
-        book = Book.objects.get(title="LION")
-        self.assertEqual(book.title, "LION")
+        url = reverse("books")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
