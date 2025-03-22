e3375e841c2c4edac92de041f90f460f676f33d6
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-22 21:43:47 +0000

    1. Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
+from django.views.generic import CreateView
+from .forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 # Create your views here.
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "blog/register.html"
+
+
+@login_required(login_url="/blog/login/")
+def profile_view(request):
+    # This view can only be accessed by authenticated users
+    return render(request, "blog/profile.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "blog/logout.html")
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
