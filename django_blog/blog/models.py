979d0036b5e6ca684119df27b53c23986a13dd25
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-23 21:01:02 +0000

    3. Adding Comment Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -18,3 +18,8 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Post, related_name="tags")
