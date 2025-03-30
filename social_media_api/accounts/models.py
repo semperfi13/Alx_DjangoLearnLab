48e6bdb422f301be6d11d22aaff28914f74fbe5d
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-03-30 15:38:54 +0000

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from rest_framework.authtoken.models import Token
 from django.conf import settings
 from django.db.models.signals import post_save
